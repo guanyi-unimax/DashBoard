c8868bd7ae0cbd360d28c03d1e364ca1f7413b03
guanyi-unimax <guanyi@unimaxtech.com.tw>
2024-06-22 13:15:51 +0800

    update

--- dist/assets/DoughnutChart-71fe151c.js
+++
@@ -1,2 +0,0 @@
-import{D as r}from"./index-e0adace4.js";import{C as e,p as n,a as p,b as s,A as i,c as u}from"./Dashboard-885c1ca0.js";import{d as l,a as m,g as c,u as g}from"./index-3d3f0f9c.js";import"./useProjects-0e78709e.js";import"./utils-b2fc192b.js";import"./ProjectStatusBadge.vue_vue_type_script_setup_true_lang-d0f0b55e.js";const A=l({__name:"DoughnutChart",props:{data:{},options:{}},setup(t){e.register(n,p,s,i,u);const o=t;return(a,d)=>(m(),c(g(r),{data:o.data,options:a.options},null,8,["data","options"]))}});export{A as default};
-//# sourceMappingURL=DoughnutChart-71fe151c.js.map
